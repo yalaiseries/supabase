cae365c006f23584327f17fa65b626cee8014995
Innovations & Integrations <youngpong@gmail.com>
2026-02-02 17:35:53 +0800

    fix: Use implicit auth flow to avoid PKCE verifier errors on password reset

--- supabase-utils.js
+++ supabase-utils.js
@@ -43,7 +43,7 @@
           persistSession: true,
           autoRefreshToken: true,
           detectSessionInUrl: true,
-          flowType: 'pkce'
+          flowType: 'implicit'
         }
       };
 
